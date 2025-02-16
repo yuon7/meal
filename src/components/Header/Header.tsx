f72627a6aca631efa6c5a46cc7f84d55ba13751c
sho0226 <s1f102201616@iniad.org>
2025-02-17 05:26:49 +0900

    feat: add hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./Header.module.css";
+import Link from "next/link";
 
-function Header() {
-  return <div className={styles.header}>Hello</div>;
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <>
+      {menuOpen && <div onClick={toggleMenu}></div>}
+
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/">Next-Hono-Template</Link>
+        </div>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/">Logout</Link>
+            </li>
+          </ul>
+
+          <button className={styles.hamburger} onClick={toggleMenu}>
+            <p></p>
+            <p></p>
+            <p></p>
+          </button>
+        </nav>
+      </header>
+
+      <div
+        className={`${styles.drawerMenu} ${menuOpen ? styles.drawerOpen : ""}`}
+      >
+        <ul>
+          <div onClick={toggleMenu}>
+            <Link href="https://nextjs.org/">Next.js</Link>
+          </div>
+          <div onClick={toggleMenu}>
+            <Link href="https://hono.dev/">Hono</Link>
+          </div>
+          <div onClick={toggleMenu}>
+            <Link href="https://supabase.com/">Supabase</Link>
+          </div>
+          <div onClick={toggleMenu}>
+            <Link href="https://www.prisma.io/">Prisma</Link>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 }
-
-export default Header;
