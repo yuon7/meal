d5dc10306e2861f7eeb9b4d0207bc4ee60806749
sho0226 <s1f102201616@iniad.org>
2025-02-24 03:55:56 +0900

    feat: complete edge runtime in Prisma

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,14 +1,35 @@
+import { PrismaClient } from "@prisma/client/edge";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+const prisma = new PrismaClient();
 
 app.get("/hello", (c) => {
   return c.json({
     message: "Hello Next.js!",
   });
+});
+
+app.get("/todos", async (c) => {
+  const todos = await prisma.todo.findMany();
+  return c.json(todos);
+});
+
+app.post("/todos", async (c) => {
+  const body = await c.req.json();
+  const { title } = body;
+  if (!title) {
+    return c.json({ error: "Title is required" }, 400);
+  }
+  const newTodos = await prisma.todo.create({
+    data: {
+      title,
+    },
+  });
+  return c.json(newTodos, 201);
 });
 
 export const GET = handle(app);
