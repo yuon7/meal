d2f8f9d9ec9f042537b8c98bd1ce140ca2a87e41
sho0226 <s1f102201616@iniad.org>
2025-02-24 01:44:32 +0900

    fix: header GUI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -59,17 +59,42 @@ export default function Header() {
         className={`${styles.drawerMenu} ${menuOpen ? styles.drawerOpen : ""}`}
       >
         <ul>
-          <div onClick={toggleMenu}>
-            <Link href="https://nextjs.org/">Next.js</Link>
-          </div>
-          <div onClick={toggleMenu}>
-            <Link href="https://hono.dev/">Hono</Link>
-          </div>
-          <div onClick={toggleMenu}>
-            <Link href="https://supabase.com/">Supabase</Link>
-          </div>
-          <div onClick={toggleMenu}>
-            <Link href="https://www.prisma.io/">Prisma</Link>
+          <div className={styles.toggleMenu}>
+            <div onClick={toggleMenu}>
+              <Link href="https://www.typescriptlang.org/" target="_blank">
+                TypeScript
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://ja.react.dev/" target="_blank">
+                React
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://nextjs.org/" target="_blank">
+                Next.js
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://hono.dev/" target="_blank">
+                Hono
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://supabase.com/" target="_blank">
+                Supabase
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://www.prisma.io/" target="_blank">
+                Prisma
+              </Link>
+            </div>
+            <div onClick={toggleMenu}>
+              <Link href="https://vercel.com/" target="_blank">
+                Vercel
+              </Link>
+            </div>
           </div>
         </ul>
       </div>
