fb0ea42e33e66804ca6db4db18575e9fce703342
sho0226 <s1f102201616@iniad.org>
2025-03-07 20:14:50 +0900

    feat: update 404

---
+++ src/app/notFoundTitle/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import styles from "./page.module.css";
+import { Container, Title, Text, Group, Button } from "@mantine/core";
+
+export default function ErrorPage() {
+  return (
+    <Container className={styles.root}>
+      <div className={styles.label}>404</div>
+      <Title className={styles.title}>You have found a secret place.</Title>
+      <Text c="dimmed" size="lg" ta="center" className={styles.description}>
+        Unfortunately, this is only a 404 page. You may have mistyped the
+        address, or the page has been moved to another URL.
+      </Text>
+      <Group justify="center">
+        <Button variant="subtle" size="md">
+          Take me back to home page
+        </Button>
+      </Group>
+    </Container>
+  );
+}
