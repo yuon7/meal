fccbf36cb27e79d2a44c5406ba030c32b366cb97
sho0226 <s1f102201616@iniad.org>
2025-03-10 23:39:12 +0900

    feat: add lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "@next/next": true,
+    },
+  },
 ];
 
 export default eslintConfig;
