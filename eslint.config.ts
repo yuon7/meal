b6c2d9956d254c1feb961a06fb98b236a2c74757
sho0226 <s1f102201616@iniad.org>
2025-03-15 01:17:10 +0900

    fix: lint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,44 +1,45 @@
-import js from '@eslint/js';
-import gitignore from 'eslint-config-flat-gitignore';
-import prettierConfig from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { files: ['**/*.ts'] },
-  gitignore({ files: ['./.gitignore',] }),
-  js.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    rules: {
-      eqeqeq: 'error',
-      'no-param-reassign': 'error',
-      "no-implicit-coercion": "error",
-      'object-shorthand': ['error', 'always'],
-      'prefer-template': 'error',
-      '@typescript-eslint/no-require-imports': 'off',
-      '@typescript-eslint/consistent-type-imports': 'error',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'error',
-      "@typescript-eslint/restrict-plus-operands": [
-        "error",
-        {
-          "allowBoolean": false,
-          "allowNullish": false,
-          "allowNumberAndString": false,
-          "allowRegExp": false,
-          "allowAny": false
-        }
-      ],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-        },
-      ],
+export default defineConfig({
+  files: ['**/*.ts', '**/*.tsx'],
+  plugins: {
+    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    react: require('eslint-plugin-react'),
+  },
+  languageOptions: {
+    parser: tsParser,
+    parserOptions: {
+      project: './tsconfig.json',
     },
   },
-  prettierConfig,
-);
+  rules: {
+    eqeqeq: 'error',
+    'no-param-reassign': 'error',
+    "no-implicit-coercion": "error",
+    'object-shorthand': ['error', 'always'],
+    'prefer-template': 'error',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    "@typescript-eslint/restrict-plus-operands": [
+      "error",
+      {
+        "allowBoolean": false,
+        "allowNullish": false,
+        "allowNumberAndString": false,
+        "allowRegExp": false,
+        "allowAny": false
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+  },
+});
