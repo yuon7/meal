e68955989ca166c9e6d07e9236573c81dc678ee2
1F10230183 <s1F102301831@iniad.org>
2025-04-15 21:23:44 +0900

    スクロールバーなくした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <ColorSchemeScript defaultColorScheme="light" />
       </head>
       <body>
