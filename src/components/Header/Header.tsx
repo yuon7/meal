e4e75f425955ada5f9e723bc1aab7ab81479dfb6
sho0226 <s1f102201616@iniad.org>
2025-05-30 22:09:28 +0900

    検索欄を導入

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,221 +1,32 @@
 "use client";
 import {
-  Anchor,
   Box,
   Burger,
-  Button,
   Center,
-  Collapse,
   Divider,
   Drawer,
   Group,
-  HoverCard,
   ScrollArea,
-  SimpleGrid,
-  Text,
-  ThemeIcon,
   UnstyledButton,
   useMantineTheme,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { User } from "@supabase/supabase-js";
-import {
-  IconBolt,
-  IconBrandNextjs,
-  IconBrandPrisma,
-  IconBrandReact,
-  IconBrandTypescript,
-  IconBrandVercel,
-  IconChevronDown,
-} from "@tabler/icons-react";
+import { IconChevronDown } from "@tabler/icons-react";
 import { UserButton } from "../UserButton/UserButton";
 import styles from "./Header.module.css";
-import { useRouter } from "next/navigation";
+import { SearchBox } from "../SearchBox/SearchBox";
 
-const techStackMockdata = [
-  {
-    icon: IconBrandTypescript,
-    title: "TypeScript",
-    description:
-      "TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.",
-    link: "https://www.typescriptlang.org/",
-  },
-  {
-    icon: IconBrandReact,
-    title: "React",
-    description:
-      "React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.",
-    link: "https://ja.react.dev/",
-  },
-  {
-    icon: IconBrandNextjs,
-    title: "Next.js",
-    description:
-      "Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.",
-    link: "https://nextjs.org/",
-  },
-  {
-    icon: IconBolt,
-    title: "Supabase",
-    description: "Supabase is an open source Firebase alternative.",
-    link: "https://supabase.com/",
-  },
-  {
-    icon: IconBrandPrisma,
-    title: "Prisma",
-    description:
-      "Ship production apps at lightning speed, and scale to a global audience effortlessly with our next-gen serverless database.",
-    link: "https://www.prisma.io/",
-  },
-  {
-    icon: IconBrandVercel,
-    title: "Vercel",
-    description:
-      "Vercel provides the developer tools and cloud infrastructure to build, scale, and secure a faster, more personalized web.",
-    link: "https://vercel.com/",
-  },
-];
-
-const dashBoardMockdata = [
-  {
-    icon: IconBolt,
-    title: "Supabase dashboard",
-    description:
-      "Supabase dashboard is a web-based interface for managing your Supabase project.",
-    link: "https://supabase.com/dashboard/projects",
-  },
-  {
-    icon: IconBrandPrisma,
-    title: "Prisma Data Platform",
-    description:
-      "Prisma Data Platform is a web-based interface for managing your Prisma project.",
-    link: "https://cloud.prisma.io/",
-  },
-];
 export function HeaderMegaMenu({ user }: { user: User | null }) {
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
     useDisclosure(false);
-  const [techStackOpened, { toggle: toggleTechStack }] = useDisclosure(false);
-  const [dashBoardOpened, { toggle: toggleDashBoard }] = useDisclosure(false);
   const theme = useMantineTheme();
-  const router = useRouter();
-
-  const techStackLinks = techStackMockdata.map((item) => (
-    <Anchor href={item.link} key={item.title} target="_blank">
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={22} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </Anchor>
-  ));
-
-  const dashBoardLinks = dashBoardMockdata.map((item) => (
-    <Anchor href={item.link} key={item.title} target="_blank">
-      <Group wrap="nowrap" align="flex-start">
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={22} color={theme.colors.blue[6]} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {item.title}
-          </Text>
-          <Text size="xs" c="dimmed">
-            {item.description}
-          </Text>
-        </div>
-      </Group>
-    </Anchor>
-  ));
 
   return (
     <Box>
       <header className={styles.header}>
-        <Group justify="space-between" h="100%">
-          <h3 style={{ cursor: "pointer" }} onClick={() => router.push("/")}>
-            Next-Hono-Template
-          </h3>
-          <Group h="100%" gap={0} visibleFrom="sm">
-            <HoverCard
-              width={600}
-              position="bottom"
-              radius="md"
-              shadow="md"
-              withinPortal
-            >
-              <HoverCard.Target>
-                <a className={styles.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Tech Stack
-                    </Box>
-                    <IconChevronDown size={16} color={theme.colors.blue[6]} />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-              <HoverCard.Dropdown style={{ overflow: "hidden" }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>Tech Stack</Text>
-                </Group>
-                <Divider my="sm" />
-                <SimpleGrid cols={2} spacing={0}>
-                  {techStackLinks}
-                </SimpleGrid>
-                <div className={styles.dropdownFooter}>
-                  <Group justify="space-between">
-                    <div>
-                      <Text fw={500} fz="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" c="dimmed">
-                        Get started with GitHub source code and your project
-                      </Text>
-                    </div>
-                    <Anchor href="https://github.com/Sho0226/Next-Hono-Template">
-                      <Button variant="default">Get started</Button>
-                    </Anchor>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard>
-
-            <HoverCard
-              width={600}
-              position="bottom"
-              radius="md"
-              shadow="md"
-              withinPortal
-            >
-              <HoverCard.Target>
-                <a className={styles.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      DashBoard
-                    </Box>
-                    <IconChevronDown size={16} color={theme.colors.blue[6]} />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-              <HoverCard.Dropdown style={{ overflow: "hidden" }}>
-                <Group justify="space-between" px="md">
-                  <Text fw={500}>dashBoard</Text>
-                </Group>
-                <Divider my="sm" />
-                <SimpleGrid cols={2} spacing={0}>
-                  {dashBoardLinks}
-                </SimpleGrid>
-              </HoverCard.Dropdown>
-            </HoverCard>
-          </Group>
-
+        <Group justify="center" h="100%">
+          <SearchBox />
           <Group visibleFrom="sm">
             <UserButton user={user} />
           </Group>
@@ -238,24 +49,22 @@ export function HeaderMegaMenu({ user }: { user: User | null }) {
       >
         <ScrollArea h="calc(100vh - 80px" mx="-md">
           <Divider my="sm" />
-          <UnstyledButton className={styles.link} onClick={toggleTechStack}>
+          <UnstyledButton className={styles.link}>
             <Center inline>
               <Box component="span" mr={5}>
-                Tech Stack
+                sample1
               </Box>
               <IconChevronDown size={16} color={theme.colors.blue[6]} />
             </Center>
           </UnstyledButton>
-          <Collapse in={techStackOpened}>{techStackLinks}</Collapse>
-          <UnstyledButton onClick={toggleDashBoard} className={styles.link}>
+          <UnstyledButton className={styles.link}>
             <Center inline>
               <Box component="span" mr={5}>
-                DashBoard
+                sample2
               </Box>
               <IconChevronDown size={16} color={theme.colors.blue[6]} />
             </Center>
           </UnstyledButton>
-          <Collapse in={dashBoardOpened}>{dashBoardLinks}</Collapse>
           <Divider my="sm" />
           <Group justify="center" grow pb="xl" px="md">
             <UserButton user={user} closeDrawer={closeDrawer} />
