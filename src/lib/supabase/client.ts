0698bf669afc7511c5789352b42cdce23b1f78ba
sho0226 <s1f102201616@iniad.org>
2025-06-07 01:29:53 +0900

    コンフリクト修正

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,3 +1,8 @@
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () => createPagesBrowserClient();
+export const createClient = () => {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+};
