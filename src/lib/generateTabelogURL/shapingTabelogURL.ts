0003124da30b5ad55ca44911bfc6303958b1aa3e
s-renren <s1f102301156@iniad.org>
2025-06-07 04:07:26 +0900

    撤退し、簡単な法のurlを作成する機能のみに

--- src/lib/generateTabelogURL/shapingTabelogURL.ts
+++
@@ -1,7 +0,0 @@
-const shapingTaelogURL = (url: string): string => {
-  const parts = url.split(/(rstLst\/)/);
-  const resultUrl = parts[0] + parts[1];
-  return resultUrl;
-};
-
-export default shapingTaelogURL;
